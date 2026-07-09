60015a70a3c8b85e4f74c00dc18c7a475f66f8a3
Mateusz Dyduch <dyduchm567@gmail.com>
2026-07-09 10:22:22 +0200

    Added new functions in function.php for title-tag, featured-image, new html5, custom-logo, custom-size-featured-image

--- HoneyTheme/functions.php
+++ HoneyTheme/functions.php
@@ -8,9 +8,28 @@ register_nav_menus(array(
     'primary' => __('Primary Menu'),
     'footer' => __('Footer Menu'),
 ));
-//post-thumbnail
+//theme_support
 function moj_motyw_setup() {
     add_theme_support('post-thumbnails');
+    add_image_size('small-thumbnail', 180, 120, true);
+    add_theme_support('title-tag');
+    add_theme_support( 'custom-logo', array(
+        'height' => 150,
+        'width'  => 300,
+        'flex-height' => true,
+        'flex-width' => true
+    ) );
+    add_theme_support( 'html5', array( 
+        'comment-list', 
+        'comment-form', 
+        'search-form', 
+        'gallery', 
+        'caption', 
+        'style', 
+        'script' 
+    ) );
+    
 }
 add_action('after_setup_theme', 'moj_motyw_setup');
+
 ?>
--- HoneyTheme/header.php
+++ HoneyTheme/header.php
@@ -3,14 +3,22 @@
 <head>
     <meta charset="<?php bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name') ?> </title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
     <header class="site-header">
         <div class="header-container">
-            <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?> </a></h1>
-            <h5><?php bloginfo('description'); ?></h5>
+            <div class="header-content-1">
+                <?php
+                    if(function_exists('the_custom_logo')){
+                        the_custom_logo();
+                    } 
+                ?>
+                <div class="header-text-content-1">
+                    <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?> </a></h1>
+                    <h5><?php bloginfo('description'); ?></h5>
+                </div>
+            </div>
             <?php if(is_page(82)){ ?>
                 <p>Thank you for viewing our work!</p>
             <?php } ?>
